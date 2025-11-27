3fecb9533bf34012e6d020e8d07de822098f638e
Kyaw Swuan Yee <k.swuan.yee@gmail.com>
2025-11-27 10:38:47 +0630

    added qol updates

---
+++ plantation_management/plantation_management/doctype/pest_and_disease/pest_and_disease.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Kyaw Swuan yee and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Pest and Disease", {
+// 	refresh(frm) {
+
+// 	},
+// });
